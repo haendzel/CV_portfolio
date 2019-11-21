cdbc1b7a0f172cb52e0fee3efdd6f32ffb135594
Filip Handzel <f.handzel@gmail.com>
2019-11-21 11:15:28 +0100

    del gallery.js, correct ouput main.min.js

--- dist/js/components/typing.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed('#typing-element', {
-    strings: ['Hello, my name is Filip Handzel.<br>I graduated from Jan Matejko Academy of Fine Arts in Krakow.<br>I like programming, design, electronic and robotic art.<br> I would like to be junior front-end developer!<br><br><br>Click <a target="_blank" rel="noopener noreferrer" href="filiphandzel-cv.pdf">here</a> to download my CV'],
-    typeSpeed: 60,
-    startDelay: 1000,
-    loop: false
-})
