f69c045d4989e3f20a397fb5adfa75da70cb9173
Filip Handzel <f.handzel@gmail.com>
2019-11-24 13:41:36 +0100

    add main.js, modules es6, typed

--- src/js/components/typing.js
+++ src/js/components/typing.js
@@ -1,7 +1,9 @@
-var typed = new Typed('#typing-element', {
+var options = {
     strings: [
         'Hello, my name is Filip Handzel.<br>I graduated from Jan Matejko Academy of Fine Arts in Krakow.<br>I like programming, design, electronic and robotic art.<br> I would like to be junior front-end developer!<br><br><br>Click <a target="_blank" rel="noopener noreferrer" href="filiphandzel-cv.pdf">here</a> to download my CV'],
     typeSpeed: 60,
     startDelay: 1000,
     loop: false
-})
+};
+
+var typed = new Typed('#typing-element', options);
---
+++ src/sass/main.js
@@ -0,0 +1,10 @@
+import jquery from 'jquery';
+import Typed from 'typed.js';
+
+window.$ = jquery;
+window.jQuery = jquery;
+
+import 'src/components/emailform';
+import 'src/components/portfolio';
+import 'src/components/toggle-menu';
+import 'src/components/typing';
